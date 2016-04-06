78c8f420b477fe114ee30a94e50ab3c0319b9af2
jemiar <hhuynh20@uic.edu>
2016-04-06 12:54:07 -0500

    Create Message.java

---
+++ Project4_NetworkChat/Server/Message.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Message implements Serializable{
+	private int type;
+	private User sender;
+	private ArrayList<User> receivers;
+	private String msg;
+	
+	public Message(int t, User s, ArrayList<User> rs, String m){
+		type = t;
+		sender = new User(s);
+		receivers = new ArrayList<User>();
+		for(int i = 0; i < rs.size(); i++)
+			receivers.add(rs.get(i));
+		msg = m;
+	}
+	
+	public int getType(){
+		return type;
+	}
+	
+	public User getUser(){
+		return sender;
+	}
+}
+ 
