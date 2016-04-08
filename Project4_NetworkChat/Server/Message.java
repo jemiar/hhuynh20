53a84a690b525e69fd97b98e8158a28c8a6a62a8
jemiar <hhuynh20@uic.edu>
2016-04-08 10:23:33 -0500

    Update Message.java

--- Project4_NetworkChat/Server/Message.java
+++ Project4_NetworkChat/Server/Message.java
@@ -9,14 +9,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Message implements Serializable{
-	private int type;					//type of message:1.Request to connect; 2.Request to disconnect; 3. Text message;
-										//4. Server response connect fail; 5. Server response connect success
-										//6. Server response disconnect fail; 7. Server response disconnect success
-										//8. Server request to add user; 9. Server request to remove user
-										//10. Request to disconnect without the need of server responding
-	private User sender;				//Sender
+	private int type;			//type of message:1.Request to connect; 2.Request to disconnect; 3. Text message;
+						//4. Server response connect fail; 5. Server response connect success
+						//6. Server response disconnect fail; 7. Server response disconnect success
+						//8. Server request to add user; 9. Server request to remove user
+						//10. Request to disconnect without the need of server responding
+	private User sender;			//Sender
 	private ArrayList<User> receivers;	//List of receiver
-	private String msg;					//text message
+	private String msg;			//text message
 	
 	//Constructor
 	public Message(int t, User s, ArrayList<User> rs, String m){
