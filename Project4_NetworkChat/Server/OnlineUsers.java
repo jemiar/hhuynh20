f19cbc5ad8b90c9215066ee771e80022e86940da
jemiar <hhuynh20@uic.edu>
2016-04-08 11:30:22 -0500

    Add comments

--- Project4_NetworkChat/Server/OnlineUsers.java
+++ Project4_NetworkChat/Server/OnlineUsers.java
@@ -1,3 +1,11 @@
+//CS 342 - SPRING 2016
+//Project 4: Network chat application
+//Developed by: Hoang Minh Huynh Nguyen (hhuynh20) and Nikolay Zakharov (nzakha2)
+
+//Class: OnlineUsers.java
+//Responsibility: manage to online users list with their corresponding socket and streams
+//Use singleton pattern to make sure no data collision
+
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -10,6 +18,7 @@ public class OnlineUsers {
 	private ArrayList<ObjectOutputStream> outputList;
 	private ArrayList<ObjectInputStream> inputList;
 	
+	//Constructor
 	private OnlineUsers(){
 		userList = new ArrayList<User>();
 		socketList = new ArrayList<Socket>();
@@ -17,12 +26,14 @@ public class OnlineUsers {
 		inputList = new ArrayList<ObjectInputStream>();
 	}
 	
+	//Get an instance of this class
 	public static synchronized OnlineUsers getInstance(){
 		if(onlineUsers == null)
 			onlineUsers = new OnlineUsers();
 		return onlineUsers;
 	}
 	
+	//Method to add new user to the list
 	public synchronized void addUser(User u, Socket s, ObjectOutputStream oos, ObjectInputStream ois){
 		userList.add(u);
 		socketList.add(s);
@@ -30,6 +41,7 @@ public class OnlineUsers {
 		inputList.add(ois);
 	}
 	
+	//Method to remove user from the list
 	public synchronized void removeUser(User u){
 		int index = -1;
 		for(int i = 0; i < userList.size(); i++)
@@ -43,18 +55,22 @@ public class OnlineUsers {
 		inputList.remove(index);
 	}
 	
+	//Method to get the list of users
 	public ArrayList<User> getUserList(){
 		return userList;
 	}
 	
+	//Method to get the list of sockets
 	public ArrayList<Socket> getSocketList(){
 		return socketList;
 	}
 	
+	//Method to get the list of output streams
 	public ArrayList<ObjectOutputStream> getOutStreamList(){
 		return outputList;
 	}
 	
+	//Method to get the list of input streams
 	public ArrayList<ObjectInputStream> getInStreamList(){
 		return inputList;
 	}
