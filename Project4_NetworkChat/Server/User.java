1c3f629ccff6e179eabae0a6533365e93dabf6de
jemiar <hhuynh20@uic.edu>
2016-04-08 10:24:08 -0500

    Update User.java

--- Project4_NetworkChat/Server/User.java
+++ Project4_NetworkChat/Server/User.java
@@ -1,20 +1,32 @@
+//CS 342 - SPRING 2016
+//Project 4: Network chat application
+//Developed by: Hoang Minh Huynh Nguyen (hhuynh20) Nikolay Zakharov (nzakha2)
+
+//Class: User.java
+//Responsibility: represent user on client machine
+
 import java.io.Serializable;
 
 public class User implements Serializable{
+	
 	private String name;
 	
+	//Constructor, parameter is a string
 	public User(String s){
 		name = s;
 	}
 	
+	//Constructor, parameter is a User object
 	public User(User u){
 		name = u.getName();
 	}
 	
+	//override toString method, used in list element display
 	public String toString(){
 		return name;
 	}
 	
+	//Method to get name instance variable of user
 	public String getName(){
 		return name;
 	}
