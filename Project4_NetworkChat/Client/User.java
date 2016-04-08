0a1b2e674436877b2b80e484d1991f8969c95702
jemiar <hhuynh20@uic.edu>
2016-04-07 20:20:14 -0500

    Update User.java

--- Project4_NetworkChat/Client/User.java
+++ Project4_NetworkChat/Client/User.java
@@ -1,12 +1,22 @@
+import java.io.Serializable;
 
-public class User {
-	public String name;
+public class User implements Serializable{
+	private String name;
 	
 	public User(String s){
 		name = s;
 	}
 	
+	public User(User u){
+		name = u.getName();
+	}
+	
 	public String toString(){
 		return name;
 	}
+	
+	public String getName(){
+		return name;
+	}
+	
 }
