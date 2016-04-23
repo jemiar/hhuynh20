d31a30c89420d46e6f5f962f4a82e6645b53c83b
jemiar <hhuynh20@uic.edu>
2016-04-23 10:26:06 -0500

    Update PlayPanel.java

--- Project5_Tetris/PlayPanel.java
+++ Project5_Tetris/PlayPanel.java
@@ -1,72 +1,25 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
-public class PlayPanel extends JPanel{
-	private boolean[][] tilesFill = new boolean[20][10];
-	private int currentX;
-	private int currentY;
+public class PlayPanel {
+	private static PlayPanel board = null;
+	private boolean[][] tiles = new boolean[Main.ROWS][Main.COLS];
 	
-	public PlayPanel(){
-		super();
-		setPreferredSize(new Dimension(300, 600));
-		for(int i = 0; i < 20; i++){
-			for(int j = 0; j < 10; j++)
-				tilesFill[i][j] = false;
-		}
-		tilesFill[0][0] = true;
-		currentX = 0;
-		currentY = 0;
-		this.addKeyListener(new KeyAdapter(){
-			public void keyPressed(KeyEvent e){
-				switch(e.getKeyCode()){
-				case KeyEvent.VK_UP:
-					if(currentY > 0){
-						tilesFill[currentY][currentX] = false;
-						currentY -= 1;
-						tilesFill[currentY][currentX] = true;
-						repaint();
-					}
-					break;
-				case KeyEvent.VK_LEFT:
-					if(currentX > 0){
-						tilesFill[currentY][currentX] = false;
-						currentX -= 1;
-						tilesFill[currentY][currentX] = true;
-						repaint();
-					}
-					break;
-				case KeyEvent.VK_DOWN:
-					if(currentY < 19){
-						tilesFill[currentY][currentX] = false;
-						currentY += 1;
-						tilesFill[currentY][currentX] = true;
-						repaint();
-					}
-					break;
-				case KeyEvent.VK_RIGHT:
-					if(currentX < 9){
-						tilesFill[currentY][currentX] = false;
-						currentX += 1;
-						tilesFill[currentY][currentX] = true;
-						repaint();
-					}
-					break;
-				default:
-					break;
-				}
-			}
-		});
+	private PlayPanel(){
+		for(int i = 0; i < Main.ROWS; i++)
+			for(int j = 0; j < Main.COLS; j++)
+				tiles[i][j] = false;
 	}
 	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		g.setColor(Color.CYAN);
-		for(int i = 0; i < 20; i++){
-			for(int j = 0; j < 10; j++){
-				if(tilesFill[i][j])
-					g.fillRect(j*30, i*30, 30, 30);
-			}
-		}
+	public static synchronized PlayPanel getInstance(){
+		if(board == null)
+			board = new PlayPanel();
+		return board;
+	}
+	
+	public boolean[][] getTiles(){
+		return tiles;
+	}
+	
+	public void setTiles(boolean[][] t){
+		tiles = t;
 	}
 }
