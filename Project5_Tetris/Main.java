afd48f4370c78cb126bdfc64b3f7d9ef0ef87ff0
jemiar <hhuynh20@uic.edu>
2016-04-30 09:59:54 -0500

    add clear line functionality

--- Project5_Tetris/Main.java
+++ Project5_Tetris/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.swing.*;
@@ -7,6 +9,10 @@ public class Main {
 	
 	public static final int ROWS = 20;
 	public static final int COLS = 10;
+	public static final boolean[] fullLine = {true, true, true, true, true, true,
+												true, true, true, true, true, true};
+	public static final boolean[] emptyLine = {true, false, false, false, false, false,
+												false, false, false, false, false, true};
 
 	public static void main(String[] args) {
 		JFrame app = new JFrame("Tetris 1.0");
@@ -29,6 +35,29 @@ public class Main {
 			tetro.getTimer().stop();
 			app.remove(tetro);
 			Grid.getInstance().setTiles();
+			
+			int min = Grid.getInstance().getMinY();
+			int[] temp = Arrays.copyOf(Grid.getInstance().getYPos(), Grid.getInstance().getYPos().length);
+			Arrays.sort(temp);
+			if(temp[0] <= min)
+				Grid.getInstance().setMinY(temp[0]);
+			
+			//After setting the piece in place, check if there are any full lines
+			//If there are, add to the arrayList lineClear
+			ArrayList<Integer> lineClear = new ArrayList<Integer>();
+			for(int i = temp[0]; i <= temp[3]; i++){
+				if(Arrays.equals(Grid.getInstance().getTiles()[i], fullLine))
+					lineClear.add(i);
+			}
+			
+			//Loop through the arraylist to clear line
+			//To update score, you can use the size of the arraylist (how many full line?)
+			if(lineClear.size() > 0){
+				for(int i = 0; i < lineClear.size(); i++){
+					int line = lineClear.get(i);
+					Grid.getInstance().drop(line);
+				}
+			}
 		}
 	}
 
