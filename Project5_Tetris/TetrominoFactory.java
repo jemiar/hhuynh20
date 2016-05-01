eac6767c6982b4a837e1f0e4834ad875799b267d
jemiar <hhuynh20@uic.edu>
2016-05-01 15:16:53 -0500

    Add comments

--- Project5_Tetris/TetrominoFactory.java
+++ Project5_Tetris/TetrominoFactory.java
@@ -1,27 +1,33 @@
+//CS 342 - SPRING 2016
+//Project 5: Tetris
+//Developed by: Hoang Minh Huynh Nguyen (hhuynh20) Nikolay Zakharov (nzakha2)
+
+//Class: TetrominoFactory.java
+//Responsibility: used to produce Tetromino objects. Factory design pattern
 
 public class TetrominoFactory {
-	public Tetromino getTetromino(int t){
+	public Tetromino getTetromino(int t, int delay){
 		switch(t){
 		case 1:
-			return new Tetromino_I();
+			return new Tetromino_I(delay);
 			
 		case 2:
-			return new Tetromino_T();
+			return new Tetromino_T(delay);
 			
 		case 3:
-			return new Tetromino_O();
+			return new Tetromino_O(delay);
 		
 		case 4:
-			return new Tetromino_L();
+			return new Tetromino_L(delay);
 			
 		case 5:
-			return new Tetromino_J();
+			return new Tetromino_J(delay);
 			
 		case 6:
-			return new Tetromino_S();
+			return new Tetromino_S(delay);
 			
 		case 7:
-			return new Tetromino_Z();
+			return new Tetromino_Z(delay);
 		
 		default:
 			return null;
