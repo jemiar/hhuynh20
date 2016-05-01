0567d9bddb74e7cd31c206ae87789bbf159a43f0
jemiar <hhuynh20@uic.edu>
2016-05-01 15:16:20 -0500

    Add comments

--- Project5_Tetris/Tetromino.java
+++ Project5_Tetris/Tetromino.java
@@ -1,6 +1,14 @@
+//CS 342 - SPRING 2016
+//Project 5: Tetris
+//Developed by: Hoang Minh Huynh Nguyen (hhuynh20) Nikolay Zakharov (nzakha2)
+
+//Class: Tetromino.java
+//Responsibility: An abstract class, that is used as a super class for all types of Tetromino
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+@SuppressWarnings("serial")
 public abstract class Tetromino extends JPanel{
 	public abstract Timer getTimer();
 }
